f1d6278a140ef03a7f7759393bfa52f12c916147
DuarteC <DuarteC@IT-WKS010.MidNet.CityOfMiddletown.com>
2019-12-03 13:24:29 -0500

    Import Classes

--- module/Training/src/Controller/TrainingConfigController.php
+++ module/Training/src/Controller/TrainingConfigController.php
@@ -1,7 +1,9 @@
 <?php
 namespace Training\Controller;
 
+use Employee\Form\UploadFileForm;
 use Midnet\Controller\AbstractConfigController;
+use Midnet\Model\Uuid;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Ddl\CreateTable;
 use Zend\Db\Sql\Ddl\DropTable;
@@ -10,6 +12,8 @@ use Zend\Db\Sql\Ddl\Column\Integer;
 use Zend\Db\Sql\Ddl\Column\Varchar;
 use Zend\Db\Sql\Ddl\Constraint\PrimaryKey;
 use Zend\View\Model\ViewModel;
+use Employee\Model\EmployeeModel;
+use Training\Model\TrainingModel;
 
 class TrainingConfigController extends AbstractConfigController
 {
@@ -22,6 +26,11 @@ class TrainingConfigController extends AbstractConfigController
     {
         $view = new ViewModel();
         $view = parent::indexAction();
+        
+        $importForm = new UploadFileForm('CLASSES');
+        $importForm->initialize();
+        $importForm->addInputFilter();
+        $view->setVariable('importForm', $importForm);
         
         $view->setTemplate('training/config');
         
@@ -94,5 +103,140 @@ class TrainingConfigController extends AbstractConfigController
         if (!file_exists('./data/files')) {
             mkdir('./data/files', 0777, true);
         }
+    }
+    
+    public function importclassesAction()
+    {
+        /****************************************
+         * Field Indexes
+         ****************************************/
+        $FNAME = 0;
+        $LNAME = 1;
+        $EMP_NUM = 2;
+        $DEPT = 3;
+        $CAT = 4;
+        $NAME = 5;
+        $DATE = 6;
+        
+        $request = $this->getRequest();
+        
+        $form = new UploadFileForm();
+        $form->initialize();
+        $form->addInputFilter();
+        
+        if ($request->isPost()) {
+            $data = array_merge_recursive(
+                $request->getPost()->toArray(),
+                $request->getFiles()->toArray()
+                );
+            
+            $form->setData($data);
+            
+            if ($form->isValid()) {
+                $uuid = new Uuid();
+                $date = new \DateTime('now',new \DateTimeZone('EDT'));
+                $today = $date->format('Y-m-d H:i:s');
+                
+                $data = $form->getData();
+                // $records = file($data['FILE']['tmp_name']);
+                
+                $row = 0;
+                if (($handle = fopen($data['FILE']['tmp_name'],"r")) !== FALSE) {
+                    while (($record = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                        $employee = new EmployeeModel($this->adapter);
+                        $result = NULL;
+                        
+                        if ($record[$EMP_NUM] === NULL) {
+                            /****************************************
+                             * No Employee ID Specified
+                             ****************************************/
+                            $result = $employee->read(['LNAME' => $record[$LNAME], ['FNAME' => $record[$FNAME]]]);
+                            if (!$result) {
+                                /****************************************
+                                 * Create Unmatched Employee
+                                 ****************************************/
+                                $employee->UUID = $uuid->generate()->value;
+                                $employee->EMP_NUM = sprintf('%06d', $employee->UUID);
+                                $employee->FNAME = $record[$FNAME];
+                                $employee->LNAME = $record[$LNAME];
+                                $employee->EMAIL = $record[$FNAME] . '.' . $record[$LNAME] . '@middletownct.gov';
+                                $employee->DATE_CREATED = $today;
+                                $employee->DATE_MODIFIED = $today;
+                                $employee->STATUS = $employee::ACTIVE_STATUS;
+                                $employee->create();
+                            }
+                        } else {
+                            if (is_numeric($record[$EMP_NUM])) {
+                                $tempEmpNum = sprintf('%06d', $record[$EMP_NUM]);
+                            } else {
+                                /****************************************
+                                 * Force Match by Name
+                                 ****************************************/
+                                $tempEmpNum = NULL;
+                            }
+                            
+                            
+                            $result = $employee->read(['EMP_NUM' => $tempEmpNum]);
+                            if (!$result) {
+                                $result = $employee->read(['LNAME' => $record[$LNAME], 'FNAME' => $record[$FNAME]]);
+                            }
+                            if (!$result) {
+                                /****************************************
+                                 * Create Unmatched Employee
+                                 ****************************************/
+                                $employee->UUID = $uuid->generate()->value;
+                                $employee->EMP_NUM = sprintf('%06d', $record[$EMP_NUM]);
+                                $employee->FNAME = $record[$FNAME];
+                                $employee->LNAME = $record[$LNAME];
+                                $employee->EMAIL = $record[$FNAME] . '.' . $record[$LNAME] . '@middletownct.gov';
+                                $employee->DATE_CREATED = $today;
+                                $employee->DATE_MODIFIED = $today;
+                                $employee->STATUS = $employee::ACTIVE_STATUS;
+                                $employee->create();
+                            }
+                        }
+                        
+                        /** I Have the Employee **/
+                        
+                        $class = new TrainingModel($this->adapter);
+                        $result = NULL;
+                        
+                        $result = $class->read(['NAME' => $record[$NAME], 'DATE_SCHEDULE' => date('Y-m-d', strtotime($record[$DATE]))  . 'T12:00']);
+                        if (!$result) {
+                            /****************************************
+                             * Create Training Class
+                             ****************************************/
+                            $class->UUID = $uuid->generate()->value;
+                            
+                            $class->CODE = sprintf('%06d', $class->UUID);
+                            $class->STATUS = $class::ACTIVE_STATUS;
+                            $class->DATE_CREATED = $today;
+                            $class->DATE_MODIFIED = $today;
+                            $class->DATE_SCHEDULE = date('Y-m-d', strtotime($record[$DATE])) . 'T12:00';
+                            $class->CATEGORY = $record[$CAT];
+                            $class->NAME = $record[$NAME];
+                            $class->create();
+                        }
+                        
+                        /** I Have the Class **/
+                        
+                        
+                        /****************************************
+                         * Assign Employee
+                         ****************************************/
+                        $class->assignEmployee($employee->UUID);
+                        
+                        $row++;
+                    }
+                    fclose($handle);
+                    unlink($data['FILE']['tmp_name']);
+                }
+                $this->flashMessenger()->addSuccessMessage("Successfully imported classes.");
+            } else {
+                $this->flashmessenger()->addErrorMessage("Form is Invalid.");
+            }
+        }
+        $url = $this->getRequest()->getHeader('Referer')->getUri();
+        return $this->redirect()->toUrl($url);
     }
 }
